56405f346635c8965816b88be302534d676e021d
MeF1sT1k <just.subway@gmail.com>
2023-10-08 19:02:24 +0300

    remove unused variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense, useState } from "react";
+import { Suspense } from "react";
 import "./styles/index.scss";
 import { useTheme } from "./theme/useTheme";
 import { classNames } from "./helpers/classNames/classNames";
